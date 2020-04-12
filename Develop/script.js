b0e6244e62da65e81fb4d9302fa8ab29cf38298c
John Leintz <johnleintz@scaledflow.com>
2020-04-12 17:59:35 -0500

    added error checking

--- Develop/script.js
+++ Develop/script.js
@@ -8,40 +8,49 @@ function promptMe() {
   var pwCharCount = prompt(
     "How many characters would you like your password to contain?"
   );
-  var specChar = confirm("Click OK to confirm including special characters");
-  var numChar = confirm("Click OK to confirm including numeric characters");
-  var lowerChar = confirm("Click OK to confirm including lowercase characters");
-  var upperChar = confirm("Click OK to confirm including uppercase characters");
-  var encrpString = buildEncrpString(specChar, numChar, lowerChar, upperChar);
-  var generatedPW = generatePW(encrpString, pwCharCount);
-  console.log("FINAL PASSWORD = " + generatedPW);
-}
-
-function buildEncrpString(specChar, numChar, lowerChar, upperChar) {
-  var specCharString = "!#$%&()*+/:;<=>?@[]^-{}~";
-  var lowerAlphaCharString = "abcdefghijklmnopqrstubwxyz";
-  var upperAlphaCharString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  var numericCharString = "0123456789";
-
-  var baseEncrypString = "";
-
-  if (specChar) {
-    baseEncrypString += specCharString;
+  if (pwCharCount >= 8 && pwCharCount <= 128) {
+    var specChar = confirm("Click OK to confirm including special characters");
+    var numChar = confirm("Click OK to confirm including numeric characters");
+    var lowerChar = confirm(
+      "Click OK to confirm including lowercase characters"
+    );
+    var upperChar = confirm(
+      "Click OK to confirm including uppercase characters"
+    );
+    var encrpString = buildEncrpString(specChar, numChar, lowerChar, upperChar);
+    var generatedPW = generatePW(encrpString, pwCharCount);
+    console.log("FINAL PASSWORD = " + generatedPW);
+    writePassword(generatedPW);
+  } else {
+    alert("You must enter a number between 8 and 128.");
   }
 
-  if (numChar) {
-    baseEncrypString += lowerAlphaCharString;
-  }
+  function buildEncrpString(specChar, numChar, lowerChar, upperChar) {
+    var specCharString = "!#$%&()*+/:;<=>?@[]^-{}~";
+    var lowerAlphaCharString = "abcdefghijklmnopqrstubwxyz";
+    var upperAlphaCharString = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var numericCharString = "0123456789";
 
-  if (lowerChar) {
-    baseEncrypString += upperAlphaCharString;
-  }
+    var baseEncrypString = "";
 
-  if (upperChar) {
-    baseEncrypString += numericCharString;
-  }
+    if (specChar) {
+      baseEncrypString += specCharString;
+    }
 
-  return baseEncrypString;
+    if (numChar) {
+      baseEncrypString += lowerAlphaCharString;
+    }
+
+    if (lowerChar) {
+      baseEncrypString += upperAlphaCharString;
+    }
+
+    if (upperChar) {
+      baseEncrypString += numericCharString;
+    }
+
+    return baseEncrypString;
+  }
 }
 
 function generatePW(encrpString, pwCharCount) {
